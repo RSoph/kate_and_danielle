c73237d6eb67e57a2846cfdd795720c702e0af1f
Kate Brender <kate.brender@gmail.com>
2014-12-23 18:16:27 -0500

    theme purchased

--- db/migrate/20141107182913_create_guests.rb
+++ db/migrate/20141107182913_create_guests.rb
@@ -1,13 +1,11 @@
 class CreateGuests < ActiveRecord::Migration
   def change
     create_table :guests do |t|
-      t.string :name
-      t.text :address
+      t.string :code
+      t.text :city
       t.string :song
       t.boolean :attending
       t.integer :guests_number
-      t.string :email
-      t.string :phone
     end
   end
 end
