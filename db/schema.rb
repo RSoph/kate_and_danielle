ecb070619ba35f1a569cee70c063af603d36e1a1
RSoph <kate.brender@gmail.com>
2015-01-26 17:27:05 -0500

    database stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141107182913) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "guests", force: true do |t|
     t.string  "code"
     t.text    "city"
